be6da4f5dc5803687f0018a7c395e5f27de08067
Анна МительковаÐ <221882@mail.ru>
2023-12-04 17:40:33 +0300

    solved zigzag algo by index diff

--- zigzag_conversion_6.py
+++ zigzag_conversion_6.py
@@ -3,25 +3,53 @@
 
 
 def convert(s, numRows):
-    s1 = s2 = s3 = s4 = ''
-    s_list = [s1, s2, s3, s4]
-    s_index = 0  # current index of s_list to fill with current symb
+
+    if numRows == 1 or len(s) == 1:
+        return s
+
+    s_list = []
+    for i in range(numRows):
+        s_list.append('') # how exactly append works?
+    diff_ind = 1  # marker to define current substring to append to
+    s_index = -1  # current index of s_list to fill with current symb
 
     for i in range(len(s)):
+        s_index += diff_ind
         s_list[s_index] += s[i]
-        if s_index < numRows:
-            s_index += 1
-        else:
-            s_index -= 1
 
-
-
+        if s_index == numRows - 1:
+            diff_ind = -1
+        
+        if s_index == 0:
+            diff_ind = 1
+    
+    res = ''
+    for i in range(numRows):
+        res += s_list[i]
+    
+    print(res)
+    return res
 
 def test():
     s = 'PAYPALISHIRING'
+    numRows = 1
+    convert(s, numRows)
+    assert convert(s, numRows) == 'PAYPALISHIRING'
+
+    s = 'P'
+    numRows = 4
+    convert(s, numRows)
+    assert convert(s, numRows) == 'P'
+
+    s = 'PAYPALISHIRING'
     numRows = 3
     convert(s, numRows)
-    # assert convert(s, numRows) == ''
+    assert convert(s, numRows) == 'PAHNAPLSIIGYIR'
+
+    s = 'PAYPALISHIRING'
+    numRows = 4
+    convert(s, numRows)
+    assert convert(s, numRows) == 'PINALSIGYAHRPI'
 
 
 if __name__ == '__main__':
