e014078c4204da807e1bc1dc6b84297dbaccc632
Анна Мителькова <221882@mail.ru>
2024-10-30 21:32:59 +0100

    finally solved

--- 5_longest_palindromic_substring.py
+++ 5_longest_palindromic_substring.py
@@ -5,83 +5,53 @@ def longestPalindrome(s):
     :type s: str
     :rtype: str
     """
-    res_palindrom = s[0]
-    curr_palindrom = ''
-
-    if len(s) == 1:
-        return res_palindrom
+    len_s = len(s)
+    longest_palindrom = getPalindrom(s, 0, 0)
     
+    for i in range(len_s - 1):
+
+        if i+1 < len_s and s[i] == s[i+1]:
+            res1 = getPalindrom(s, i, i+1)
+            res2 = getPalindrom(s, i, i)
+            res = res1 if  len(res1) > len(res2) else res2
+        else:
+            res = getPalindrom(s, i, i)
+        if len(res) > len(longest_palindrom):
+            longest_palindrom = res
+    
+    return longest_palindrom
 
 
-    # for i in range(len(s) - 1):
-        # print(f'curr ind: {i}')
-
-        # if s[i] == s[i+1]:
-        #     if i+2 < len(s) and s[i] == s[i+2]:
-        #         curr_palindrom = s[i:i+3]
-        #         left = i - 1
-        #         right = i + 3 
-        #     else:
-        #         curr_palindrom =  s[i:i+2]
-        #         left = i - 1
-        #         right = i + 2        
-        # else:
-        #     curr_palindrom = s[i]
-        #     left = i - 1
-        #     right = i + 1
-        
-        
-        # while left > -1 and right < len(s):
-        #     if s[left] == s[right]:
-        #         curr_palindrom = s[left : right + 1]
-        #     else:
-        #         break
-        #     left -= 1
-        #     right += 1
-
-    for i in range(1, len(s)):
-        left = i - 1
-        right = i + 1
-
-        if s[i] == s[right]:
-            right += 1
-        
-        while left > -1 and right < len(s):
-            if s[left] == s[right]:
-                curr_palindrom = s[left : right + 1]
-            else:
-                break
+def getPalindrom(s, i, j):
+    left = i - 1
+    right = j + 1
+    res = s[i:j+1]
+    while left > -1 and right < len(s):
+        if s[left] == s[right]:
+            res = s[left: right + 1]
             left -= 1
             right += 1
-
-
-
-
-        
-        print(curr_palindrom)
-
-        if len(curr_palindrom) > len(res_palindrom):
-            res_palindrom = curr_palindrom
+        else:
+            break
     
-    return res_palindrom
+    return res
 
              
 
 def test():
-    # s = 'babad'
-    # assert longestPalindrome(s) == 'bab'
+    s = 'SQQSYYSQQS'
+    assert longestPalindrome(s) == 'SQQSYYSQQS'
     
-    # s = 'cbbd'
-    # assert longestPalindrome(s) == 'bb'
-
-    # s = 'abaab'
-    # assert longestPalindrome(s) == 'baab'
-
-    # s = 'baab'
-    # assert longestPalindrome(s) == 'baab'
+    s = 'aaaaa'
+    assert longestPalindrome(s) == 'aaaaa'
 
     s = 'aaaa'
     assert longestPalindrome(s) == 'aaaa'
+    
+    s = 'cbbc'
+    assert longestPalindrome(s) == 'cbbc'
+
+
 
     
 
@@ -91,4 +61,6 @@ if __name__ == '__main__':
 
 
 # i need the longest palindrom string
+# a - center
+# aa - center
 
