94789f1f60d5b1a7f12f1eecc663148f7e6ecd22
Анна Мителькова <221882@mail.ru>
2024-11-12 23:02:03 +0100

    hurray

--- 836_rectangle_overlap.py
+++ 836_rectangle_overlap.py
@@ -6,21 +6,29 @@ def isRectangleOverlap(rec1, rec2):
     :type rec1: List[int]
     :type rec2: List[int]
     :rtype: bool
-    """   
-    x_intersect = rec2[0] < rec1[2] < rec2[2] or rec1[0] < rec2[2] < rec1[2]
-    y_intersect = rec2[1] < rec1[3] < rec2[3] or rec1[1] < rec2[3] < rec1[3]
+    """
+    
+    x_intersect = rec2[0] < rec1[2] <= rec2[2] or rec1[0] < rec2[2] <= rec1[2]
+    y_intersect = rec2[1] < rec1[3] <= rec2[3] or rec1[1] < rec2[3] <= rec1[3]
+
+
     print(x_intersect and y_intersect)
+
     return x_intersect and y_intersect
         
 
 def test():
     # x1, y1, x2, y2
-    rec1 = [0,0,2,2]
-    rec2 = [1,1,3,3]
-    assert isRectangleOverlap(rec1, rec2) == True
+    # rec1 = [0,0,2,2]
+    # rec2 = [1,1,3,3]
+    # assert isRectangleOverlap(rec1, rec2) == True
 
-    rec1 = [0,0,1,1]
-    rec2 = [1,0,2,1]
+    # rec1 = [0,0,1,1]
+    # rec2 = [1,0,2,1]
+    # assert isRectangleOverlap(rec1, rec2) == False
+
+    rec1 = [5,15,8,18]
+    rec2 = [0,3,7,9]
     assert isRectangleOverlap(rec1, rec2) == False
 
 
