5f5f43d58e298b2b6a1a10d2c71ae404f01e06eb
Анна Мителькова <221882@mail.ru>
2024-11-15 14:59:31 +0100

    still too long

--- 49_group_anagrams.py
+++ 49_group_anagrams.py
@@ -16,43 +16,33 @@ def groupAnagrams(strs):
     :type strs: List[str]
     :rtype: List[List[str]]
     """
-    res_list = []    
-    curr_anagrams_list = [strs.pop(0)]
+    res_list = []
 
-    while len(strs):        
+    while len(strs) > 0:
+        curr_anagrams_list = [strs.pop(0)]
         for s in strs:
-            print(s)
-            if isAnagram(s, curr_anagrams_list[-1]):
+            if isAnagram(s, curr_anagrams_list[0]):
                 curr_anagrams_list.append(s)
         
         for el in curr_anagrams_list:
             if el in strs:
-                strs.pop(strs.index(el))
-        
-        
-        if len(strs) > 0:
-            res_list.append(curr_anagrams_list)
-            curr_anagrams_list = [strs.pop(0)]
-        continue
-
-    if len(strs) == 0:
+                strs.remove(el)
         res_list.append(curr_anagrams_list)
-            
-    print(res_list)
+
     return res_list
+              
+
+
+
+    
 
 
 def test():
-    # L = [["bat"],["nat","tan"],["ate","eat","tea"]]
-    # print('bat' in L[0])
-    # print(['bat'] in L)
-    # print(['nat'] in L)
     strs = ["",""]
     assert groupAnagrams(strs) == [["", ""]]
 
     strs = ["eat","tea","tan","ate","nat","bat"]
     assert groupAnagrams(strs) == [["bat"],["nat","tan"],["ate","eat","tea"]]
-    # groupAnagrams(strs)
 
 
 if __name__ == '__main__':
