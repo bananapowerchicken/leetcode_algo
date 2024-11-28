3057a8c4df41b9cd7ec0ddc9672d556f236660d2
Анна Мителькова <221882@mail.ru>
2024-11-28 13:19:48 +0100

    queue kinda works

--- 232_implement_queue_using_stacks.py
+++ 232_implement_queue_using_stacks.py
@@ -1,51 +1,66 @@
 # https://leetcode.com/problems/implement-queue-using-stacks/description/
 # easy
 
+# leftmost is front of the queue
+
 
 class MyQueue:
 
     def __init__(self):
-        pass
+        self.queue = []
         
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
-        """
-        
 
-    def pop(self):
+        Pushes element x to the back of the queue
+        """
+        self.queue.append(x)
+
+    def pop(self) -> int:
         """
         :rtype: int
+
+        Removes the element from the front of the queue and returns it
         """
+        front_el = self.queue[0]
+        self.queue.remove(front_el)
+        return front_el
         
 
-    def peek(self):
+    def peek(self) -> int:
         """
         :rtype: int
+
+        Returns the element at the front of the queue - the left one
         """
+        return self.queue[0]
         
 
-    def empty(self):
+    def empty(self) -> bool:
         """
         :rtype: bool
+
+        Returns true if the queue is empty, false otherwise
         """
+        return self.queue == []
 
 
 def test():
-    pass
-    # test_stack = MinStack()
-    # test_stack.push(-2)
-    # test_stack.push(0)
-    # test_stack.push(-3)
-    # assert test_stack.getMin() == -3
-    # print(test_stack.stack)
+    test_queue = MyQueue()
 
-    # test_stack.pop()
-    # print(test_stack.stack)
-    # assert test_stack.top() == 0
-    # assert test_stack.getMin() == -2
+    test_queue.push(1)
+    test_queue.push(2)
+    print(test_queue.queue)
+
+    assert test_queue.peek() == 1
+    assert test_queue.pop() == 1
+    print(test_queue.queue)
+
+    assert test_queue.empty() == False
+
 
     
 
