5bba4131ffcac07ba46ff0bc6dc84293d2c14d3d
Анна Мителькова <221882@mail.ru>
2024-11-28 13:05:36 +0100

    new algo create queue

---
+++ 232_implement_queue_using_stacks.py
@@ -0,0 +1,63 @@
+# https://leetcode.com/problems/implement-queue-using-stacks/description/
+# easy
+
+
+class MyQueue:
+
+    def __init__(self):
+        pass
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+
+
+def test():
+    pass
+    # test_stack = MinStack()
+    # test_stack.push(-2)
+    # test_stack.push(0)
+    # test_stack.push(-3)
+    # assert test_stack.getMin() == -3
+    # print(test_stack.stack)
+
+    # test_stack.pop()
+    # print(test_stack.stack)
+    # assert test_stack.top() == 0
+    # assert test_stack.getMin() == -2
+
+    
+
+
+
+if __name__ == "__main__":
+    test()
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
