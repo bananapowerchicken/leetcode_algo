c2a933779c2cff07352fb92b6d2dc16bbcd71342
Анна Мителькова <221882@mail.ru>
2024-12-02 16:58:26 +0100

    start new queue task

---
+++ 622_design_circular_queue.py
@@ -0,0 +1,60 @@
+# https://leetcode.com/problems/design-circular-queue/description/
+# medium
+
+class MyCircularQueue():
+
+    def __init__(self, k):
+        """
+        :type k: int
+
+        Initializes the object with the size of the queue to be k
+        """
+        queue = [] * k
+        
+
+    def enQueue(self, value):
+        """
+        :type value: int
+        :rtype: bool
+        """
+        
+
+    def deQueue(self):
+        """
+        :rtype: bool
+        """
+        
+
+    def Front(self):
+        """
+        :rtype: int
+        """
+        
+
+    def Rear(self):
+        """
+        :rtype: int
+        """
+        
+
+    def isEmpty(self):
+        """
+        :rtype: bool
+        """
+        
+
+    def isFull(self):
+        """
+        :rtype: bool
+        """
+        
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
